ccb9ecbe88d9c00a079c58ef8b211d15aaca3e92
Arthur Pedroti <arthurpedroti@gmail.com>
2022-05-06 14:19:16 -0300

    reacr query setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,28 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
+import { makeServer } from '../services/mirage'
 import { theme } from '../styles/theme'
 
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient()
+
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,6 +5,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -19,12 +20,16 @@ import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
+import { useUsers } from '../../hooks/useUsers'
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   })
+
+  const { data, isLoading, isFetching, error } = useUsers()
+
   return (
     <Box>
       <Header />
@@ -36,6 +41,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
@@ -51,126 +59,72 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th w="8" />
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Arthur Pedroti</Text>
-                    {isWideVersion && (
-                      <Text fontSize="sm" color="gray.300">
-                        arthurpedroti@gmail.com
-                      </Text>
-                    )}
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    borderRadius="full"
-                    leftIcon={
-                      <Icon
-                        as={RiPencilLine}
-                        mr={isWideVersion ? '0' : '-3'}
-                        ml={isWideVersion ? '0' : '-1'}
-                        fontSize="16"
-                      />
-                    }
-                  >
-                    {isWideVersion ? 'Editar' : ''}
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Arthur Pedroti</Text>
-                    {isWideVersion && (
-                      <Text fontSize="sm" color="gray.300">
-                        arthurpedroti@gmail.com
-                      </Text>
-                    )}
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    borderRadius="full"
-                    leftIcon={
-                      <Icon
-                        as={RiPencilLine}
-                        mr={isWideVersion ? '0' : '-3'}
-                        ml={isWideVersion ? '0' : '-1'}
-                        fontSize="16"
-                      />
-                    }
-                  >
-                    {isWideVersion ? 'Editar' : ''}
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Arthur Pedroti</Text>
-                    {isWideVersion && (
-                      <Text fontSize="sm" color="gray.300">
-                        arthurpedroti@gmail.com
-                      </Text>
-                    )}
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    borderRadius="full"
-                    leftIcon={
-                      <Icon
-                        as={RiPencilLine}
-                        mr={isWideVersion ? '0' : '-3'}
-                        ml={isWideVersion ? '0' : '-1'}
-                        fontSize="16"
-                      />
-                    }
-                  >
-                    {isWideVersion ? 'Editar' : ''}
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th w="8" />
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data?.map((user) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={['4', '4', '6']}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            {isWideVersion && (
+                              <Text fontSize="sm" color="gray.300">
+                                {user.email}
+                              </Text>
+                            )}
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                        <Td>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="purple"
+                            borderRadius="full"
+                            leftIcon={
+                              <Icon
+                                as={RiPencilLine}
+                                mr={isWideVersion ? '0' : '-3'}
+                                ml={isWideVersion ? '0' : '-1'}
+                                fontSize="16"
+                              />
+                            }
+                          >
+                            {isWideVersion ? 'Editar' : ''}
+                          </Button>
+                        </Td>
+                      </Tr>
+                    )
+                  })}
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
